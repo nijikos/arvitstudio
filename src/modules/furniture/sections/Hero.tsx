bb23382d9c20e0024a59d4f5381e1db41a6a278e
nijikos <nijikos@gmail.com>
2024-11-29 12:25:11 +0700

    update hero image: use background overlay instead

--- src/modules/furniture/sections/Hero.tsx
+++ src/modules/furniture/sections/Hero.tsx
@@ -19,28 +19,22 @@ export default function Hero({ test }: HeroProps) {
       {/* --------- Navigation Bar */}
       <NavigationBar />
       {/* --------- HERO IMAGE */}
-      <Image
-        alt='hero furniture image'
-        src={`/images/furniture/hero-furniture-4.png`}
-        width={1024}
-        height={1024}
-        className='w-[60vw] h-[76vh] lg:h-[600px] md:w-full absolute right-0 bottom-0 object-cover rounded-tl-[80px] md:rounded-none'
-      />
+      <div className='w-[60vw] h-[76vh] lg:h-[600px] md:w-full absolute right-0 bottom-0 rounded-tl-[80px] md:rounded-none hero-img'></div>
       {/* --------- HERO TEXT */}
-      <div className='z-10 absolute left-0 bottom-0 flex flex-col gap-4 text-white md:text-primary-dark pl-40 3xl:pl-20 lg:px-6 pb-12'>
-        <p className='w-[820px] lg:w-fit font-semibold text-8xl lg:text-5xl drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)] md:drop-shadow-none'>
+      <div className='z-10 absolute left-0 bottom-0 flex flex-col gap-4 text-white pl-40 3xl:pl-20 lg:px-6 pb-12'>
+        <p className='w-[820px] lg:w-fit font-semibold text-8xl lg:text-5xl drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)]'>
           Furniture Design & Build
         </p>
         <div>
-          <p className='font-light lg:text-sm lg:w-fit drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)] md:drop-shadow-none'>
+          <p className='font-light lg:text-sm lg:w-fit drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)]'>
             Hemat waktu, biaya, dan energi.
           </p>
-          <h1 className='font-light lg:text-sm lg:w-fit drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)] md:drop-shadow-none'>
+          <h1 className='font-light lg:text-sm lg:w-fit drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)]'>
             Arvit Studio mewujudkan hunian impian Anda secara TEPAT, EFISIEN,
             ESTETIK
           </h1>
         </div>
-        <div className='flex flex-row gap-2 items-center text-accent md:text-black'>
+        <div className='flex flex-row gap-2 items-center text-accent md:text-accent-white'>
           <Location className='lg:w-5' />
           <p className='lg:text-xs '>Jabodetabek</p>
         </div>
@@ -54,7 +48,7 @@ export default function Hero({ test }: HeroProps) {
           </Link>
           <Link
             href='#portfolio'
-            className='button-lg lg:button-md lg:text-sm button-outline-white md:button-outline-primary'
+            className='button-lg lg:button-md lg:text-sm button-outline-white'
           >
             Lihat Portfolio
           </Link>
