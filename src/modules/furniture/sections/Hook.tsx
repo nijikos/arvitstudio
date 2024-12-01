45990d7c34839634630bba24c359698ad4dde01f
nijikos <nijikos@gmail.com>
2024-12-01 23:05:22 +0700

    add image to hook

--- src/modules/furniture/sections/Hook.tsx
+++ src/modules/furniture/sections/Hook.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 type HookProps = {
@@ -6,9 +7,11 @@ type HookProps = {
 
 export default function Hook({ test }: HookProps) {
   const questions = [
-    "- Sulit Menemukan Furnitur yang Pas Untuk Ruang Kecil atau Sudut Khusus?",
-    "- Sulit menemukan desain interior yang pas dengan gaya dan kenyamanan rumah Anda?",
-    "- atau bahkan anda Kesulitan Mencari Furnitur yang Tahan Lama dan Berkualitas?",
+    "Sulit Menemukan Furnitur yang Pas Untuk Ruang Kecil atau Sudut Khusus?",
+    "Sulit menemukan desain interior yang pas dengan gaya dan kenyamanan rumah Anda?",
+    "atau bahkan anda Kesulitan Mencari Furnitur yang Tahan Lama dan Berkualitas?",
+  ];
+  const solutions = [
     <span key={4} className='!text-3xl font-semibold'>
       Solusi semua masalah anda{" "}
       <span className='font-bold text-error !text-3xl'>
@@ -36,22 +39,44 @@ export default function Hook({ test }: HookProps) {
     </span>,
   ];
   return (
-    <div
-      id='solusi'
-      className='w-full flex items-center justify-center py-24 px-32 lg:px-6 bg-accent-lightest/40'
-    >
-      <div className='flex-1 max-w-[1200px] flex flex-col items-center justify-center gap-8'>
-        <div className='flex flex-col items-center justify-center gap-12 text-primary relative'>
-          {questions.map((q, ii) => {
-            return (
-              <p
-                className='text-left max-w-[760px] w-fit text-2xl lg:text-xl'
-                key={ii}
-              >
-                {q}
-              </p>
-            );
-          })}
+    <div id='solusi'>
+      <div className='w-full flex items-center justify-center lg:justify-start sm:justify-center pt-24  px-32 lg:px-36 md:px-6 bg-primary overflow-y-visible'>
+        <div className='flex-1 max-w-[1200px] lg:max-w-[320px] flex flex-col items-center justify-center gap-8'>
+          <ul className='list-disc pb-24 sm:pb-16 flex flex-col items-center justify-center gap-6 text-white relative'>
+            {questions.map((q, ii) => {
+              return (
+                <li
+                  className='relative text-left max-w-[760px] w-fit text-2xl lg:text-xl sm:drop-shadow-[0_1px_3px_rgba(0,0,0,0.5)] z-10'
+                  key={ii}
+                >
+                  {q}
+                </li>
+              );
+            })}
+            <Image
+              className='absolute left-full bottom-0 sm:-right-20 sm:left-auto max-h-[520px] w-auto sm:max-h-[200px]'
+              src={`/images/hook/hook-2.png`}
+              alt='Hook Image'
+              width={894}
+              height={1535}
+            />
+          </ul>
+        </div>
+      </div>
+      <div className='w-full flex items-center justify-center py-24 px-32 lg:px-6 bg-accent-lightest/40'>
+        <div className='flex-1 max-w-[1200px] flex flex-col items-center justify-center gap-8'>
+          <div className='flex flex-col items-start justify-center gap-12 text-primary relative'>
+            {solutions.map((q, ii) => {
+              return (
+                <p
+                  className='text-left max-w-[760px] w-fit text-2xl lg:text-xl'
+                  key={ii}
+                >
+                  {q}
+                </p>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
