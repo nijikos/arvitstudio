5c35cf3432bb294d6435ec4f30df9e92d4717675
nijikos <nijikos@gmail.com>
2024-12-03 13:30:34 +0700

    update Team Profile Label

--- src/modules/shared/components/TimProfilePicture.tsx
+++ src/modules/shared/components/TimProfilePicture.tsx
@@ -25,14 +25,17 @@ export default function TimProfilePicture({
         alt='Portfolio Image'
         blurDataURL='/images/defaultimg.jpg'
       />
-      <div className='bg-primary-darker text-white px-8 sm:px-4 py-6 w-fit absolute bottom-6 right-0 border-t-4 border-accent flex flex-col gap-2 items-end'>
-        <p className='text-xl sm:text-lg font-semibold'>
+      <div className='bg-primary-darker text-white px-8 sm:px-4 py-6 w-max absolute bottom-6 right-1/2 translate-x-1/2 border-t-4 border-accent flex flex-col gap-2 items-center'>
+        <p className='text-2xl sm:text-lg font-semibold'>
           Jaila Muhardila S.Ars
         </p>
-        <p className='sm:text-base'>(founder Arvit Studio)</p>
-        <div className='flex flex-row items-center md:items-start gap-4 sm:gap-2 bg-accent text-primary-dark w-fit px-3 py-2 rounded-full md:rounded-none'>
+        <p className='text-base'>(founder Arvit Studio)</p>
+        <div className='flex flex-row items-center md:items-start gap-4 sm:gap-2 bg-accent text-primary-dark w-fit px-2 py-1 rounded-xl md:rounded-none'>
           <Teacher size={16} className='shrink-0' />
-          <p>Arsitek Interior - Universitas Indonesia{`'`} 2012</p>
+          <div>
+            <p>Arsitek Interior</p>
+            <p>Universitas Indonesia{`'`} 2012</p>
+          </div>
         </div>
       </div>
     </div>
